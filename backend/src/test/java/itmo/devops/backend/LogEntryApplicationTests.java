227ce252880abf318d5f023d557c1749fb3ee4d0
Pavel Popov <pashafizmat1@gmail.com>
2025-03-14 22:25:51 +0300

    CI + test template

--- backend/src/test/java/itmo/devops/backend/LogEntryApplicationTests.java
+++ backend/src/test/java/itmo/devops/backend/LogEntryApplicationTests.java
@@ -4,11 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LogEntryApplicationTests {
@@ -17,8 +12,9 @@ class LogEntryApplicationTests {
 	private TestRestTemplate restTemplate;
 
 	@Test
-	void testCreateProduct() {}
+	void testCreateLogEntry() {
 
-	@Test
-	void testGetProductById() {}
+	}
+
+
 }
