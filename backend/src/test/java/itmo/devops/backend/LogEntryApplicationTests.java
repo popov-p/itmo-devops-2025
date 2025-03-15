2824da1024ac0029496017893656a61c79f28283
Pavel Popov <pashafizmat1@gmail.com>
2025-03-15 21:33:13 +0300

    get, post tests

--- backend/src/test/java/itmo/devops/backend/LogEntryApplicationTests.java
+++ backend/src/test/java/itmo/devops/backend/LogEntryApplicationTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LogEntryApplicationTests {
@@ -19,7 +23,55 @@ class LogEntryApplicationTests {
 		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		assert response.getStatusCode().value() == 404;
-		
+
 		System.out.println("Response: " + response.getBody());
+	}
+
+	@Test
+	void testCreateLogEntry() {
+		String url = "http://127.0.0.1:8070/api/logentries";
+
+		String jsonBody = "{\n" +
+				"    \"employeeName\": \"Pavel Popov\",\n" +
+				"    \"logMessage\": \"Hello world\"\n" +
+				"}";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+
+		HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+
+		ResponseEntity<String> response = restTemplate
+				.postForEntity(url, entity, String.class);
+
+		assertEquals(201, response.getStatusCode().value());
+
+		System.out.println("Response: " + response.getBody());
+	}
+
+	@Test
+	void testGetLogEntriesNotEmptyDB() {
+		String url = "http://127.0.0.1:8070/api/logentries";
+
+		String jsonBody = "{\n" +
+				"    \"employeeName\": \"Pavel Popov\",\n" +
+				"    \"logMessage\": \"Hello world\"\n" +
+				"}";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+
+		HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+
+		ResponseEntity<String> post_response = restTemplate
+				.postForEntity(url, entity, String.class);
+
+		assertEquals(201, post_response.getStatusCode().value());
+		
+		ResponseEntity<String> get_response = restTemplate.getForEntity(url, String.class);
+
+		assert get_response.getStatusCode().value() == 200;
+
+		System.out.println("Response: " + get_response.getBody());
 	}
 }
