2e5fa9a7561b23f9d3f35a7c25b292e62f404c27
popov-p <pashafizmat1@gmail.com>
2025-04-25 23:14:35 +0300

    env vars deletion

--- set-sonar-env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export SONAR_PROJECT_KEY="popov-p_itmo-devops-2025"
-export SONAR_HOST_URL="https://sonarcloud.io"
-export SONAR_TOKEN="735eda77ae81938cde13c458c7775ce0d997cb34"
-export SONAR_ORGANIZATION_KEY="popov-p"
